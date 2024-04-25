964d50e384ae7840eab542e7199cc6257af4dadb
erbilalahmad <bilalahmad876923@gmail.com>
2024-04-25 17:16:43 +0530

    database added

--- newbull/src/config/connectDB.js
+++ newbull/src/config/connectDB.js
@@ -13,10 +13,10 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'bigbull',
+    host: '89.116.33.117',
+    user: 'newbull',
+    password: 'newbull',
+    database: 'newbull',
     waitForConnections: true,
     connectionLimit: 50,
     queueLimit: 0
